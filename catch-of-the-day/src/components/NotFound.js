41e1d963de63345d56e36802b8d59b88b8938fe7
Michael Su <xmichael.su@gmail.com>
2019-08-22 15:27:14 -0700

    React Router setup - to redircted url path

---
+++ catch-of-the-day/src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const notFound = () => {
+  return (
+    <div>
+      <h1>404 Store Not Found!</h1>;
+    </div>
+  );
+};
+
+export default notFound;
---
+++ catch-of-the-day/src/components/Router.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import StorePicker from './StorePicker';
+import App from './App';
+import NotFound from './NotFound';
+
+const Router = () => (
+  <BrowserRouter>
+    {/* Switch tries Routes set, in order - then it eventually goes to not found */}
+    <Switch>
+      <Route exact path="/" component={StorePicker} />
+      {/* Catch all Route to any store name */}
+      <Route exact path="/store/:storeID" component={App} />
+      {/* Last route for everything else, omit the path prop */}
+      <Route component={NotFound} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Router;
