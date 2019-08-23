fdab09693df5d73a21dfab04d288d1e578560743
Michael Su <xmichael.su@gmail.com>
2019-08-23 14:40:25 -0700

    Dynamically render Fishes from State to the page

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -3,6 +3,7 @@ import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
 import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends React.Component {
   state = {
@@ -28,7 +29,13 @@ class App extends React.Component {
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
-          <ul className="fishes" />
+          <ul className="fishes">
+            <li>
+              {Object.keys(this.state.fishes).map(key => (
+                <Fish key={key} fishData={this.state.fishes[key]} />
+              ))}
+            </li>
+          </ul>
         </div>
         <Order />
         <Inventory
