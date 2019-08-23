d596baa0abb7073250f8d8ce42ab89d0178c88be
Michael Su <xmichael.su@gmail.com>
2019-08-22 23:30:55 -0700

    Implement State to contain Fish from AddFishForm

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,20 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = fish => {
+    //1. take a copy of the exisiting state
+    const fishes = { ...this.state.fishes };
+    //2. add our new fishes
+    fishes[`fish${Date.now()}`] = fish;
+    //3. set the new fishes object to state
+    this.setState({ fishes });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +25,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }
